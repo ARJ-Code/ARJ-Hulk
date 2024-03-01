b32b5644d0ccdc0b1115c7df0fb806f62ca16ef5
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-03-01 13:50:42 -0500

    parse result

--- src/test.py
+++ src/test.py
@@ -1,12 +1,16 @@
 from hulk_parser.grammar import Grammar
 
-g=Grammar()
+# g=Grammar()
 
-g.add_main("S")
-g.add_production("S", ["A B","S B","EOF"])
-g.calculate_first()
-g.calculate_follow()
+# g.add_main("S")
+# g.add_production("S", ["A B","S B","EOF"])
+# g.calculate_first()
+# g.calculate_follow()
 
-for i in g.follows:
-    for j in g.follows[i]:
-        print(i, j)
+# for i in g.follows:
+#     for j in g.follows[i]:
+#         print(i, j)
+
+a=[1,2]
+a.reverse()
+print(a)
