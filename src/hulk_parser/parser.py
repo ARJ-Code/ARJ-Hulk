3cb6dd080ff9adaa22e16262acebb5a4fe21a3d3
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-03-01 13:57:26 -0500

    parse result action

--- src/hulk_parser/parser.py
+++ src/hulk_parser/parser.py
@@ -1,6 +1,7 @@
 from .grammar import Grammar, GrammarToken, EOF
-from .automatonLR import AutomatonLR,Action
+from .automatonLR import AutomatonLR, Action
 from typing import List
+from .parse_out import ParseResult
 
 
 class Parser:
@@ -8,31 +9,39 @@ class Parser:
         self.grammar: Grammar = grammar
         self.aut: AutomatonLR = automatonLR
 
-    def parse(self, tokens: List[GrammarToken]):
+    def parse(self, tokens: List[GrammarToken]) -> ParseResult:
         tokens.append(EOF())
+        productions_result: List[GrammarToken] = []
 
         index: int = 0
         stack_tokens: List[GrammarToken] = []
-      
+
         while True:
             action, ind = self.aut.action(
                 stack_tokens, tokens[index])
-            
+
             if action == Action.SHIFT:
                 self.shift_action(stack_tokens, tokens[index])
-                index+=1
-            
-            if action == Action.REDUCE:
-                self.reduce_action(stack_tokens, ind)
+                index += 1
 
-            
+            if action == Action.REDUCE:
+                self.reduce_action(stack_tokens, ind, productions_result)
+
+            if action == Action.ERROR:
+                return ParseResult(error=index)
+
+            if action == Action.ACCEPT:
+                productions_result.reverse()
+                return ParseResult(derivations=productions_result)
 
     def shift_action(self, stack_tokens: List[GrammarToken], input: GrammarToken):
         stack_tokens.append(input)
 
-    def reduce_action(self, stack_tokens: List[GrammarToken], ind: int):
+    def reduce_action(self, stack_tokens: List[GrammarToken], ind: int, productions_result: List[GrammarToken]):
         for production in self.grammar.productions:
             if production.ind == ind:
+                productions_result.append(production)
+
                 for _ in range(len(production.body)):
                     stack_tokens.pop()
                 stack_tokens.append(production.head)
