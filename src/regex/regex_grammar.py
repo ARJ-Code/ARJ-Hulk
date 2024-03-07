2e35dfc4da065ba82551c23c2c06e99a93601a5a
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-03-06 21:59:01 -0500

    grammar

---
+++ src/regex/regex_grammar.py
@@ -0,0 +1,37 @@
+from parser.grammar import Grammar, GrammarToken
+from .regex_core import RegexToken
+
+regex_special_tokens = ['?', '+', '*', '^',
+                        '$', '[', ']', '(', ')', '{', '}', '.']
+
+
+class RegexGrammar():
+    def __init__(self) -> None:
+        self.grammar = self.__get_grammar()
+
+    def __get_grammar(self) -> Grammar:
+        g = Grammar()
+
+        g.add_main('S')
+
+        g.add_production('S', ['E'])
+        g.add_production('E', ['A | E', 'A'])
+        g.add_production('A', ['B C D'])
+        g.add_production('B', ['^', ''])
+        g.add_production('D', ['$', ''])
+        g.add_production('C', ['F C', 'F'])
+        g.add_production('F', ['[ G ]', 'H I'])
+        g.add_production(
+            'I', ['?', '+', '*', '{ ch }', '{ ch , ch }', '{ , ch }', '{ ch , }'])
+        g.add_production('H', ['ch', '( E )', '.'])
+        g.add_production('G', ['^ J', 'J'])
+        g.add_production('J', ['K J', 'K'])
+        g.add_production('K', ['ch', 'ch - ch'])
+
+        return g
+
+    def regex_to_grammar(self, token: RegexToken) -> GrammarToken:
+        if token.is_special:
+            return [t for t in self.grammar.non_terminals if t.value == token.value][0]
+
+        return [t for t in self.grammar.non_terminals if t.value == 'ch'][0]
